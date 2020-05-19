e419dcab8d082430fa2c71a842fd6bfc169b78c5
QuangThuanLQT <thuanquangle97@gmail.com>
2020-05-20 00:16:41 +0700

    lỗi do sale_invoicing

--- odoo-11.0/ACode/sale_invoicing/models/models.py
+++ odoo-11.0/ACode/sale_invoicing/models/models.py
@@ -16,58 +16,44 @@ class Customer_Credit_Notes(models.Model):
     _inherit = 'account.invoice'
 
 
-class Sales_Receipts(models.Model):
-    _inherit = 'account.voucher'
+# class Sales_Receipts(models.Model):
+#     _inherit = 'account.voucher'
 
-    amount_input = fields.Float('Số tiền')
-    note = fields.Char(string='Ghi chú')
-
-    journal_id = fcollect_type = fields.Selection([('journal', 'Customer Invoices (VNĐ)')], string='Journal',
-                                                  default='journal')
-
-    payment_journal_id = fields.Many2one('account.journal', string='Payment Method')
-    account_id = fields.Many2one('account.account', string='Account')
-
-    number_voucher = fields.Char(string='Số phiếu thu')
-    name = fields.Char(string='Payment Reference')
-    collect_type = fields.Selection([('sale', 'Collect Sale')], string='Collect Type', default='sale')
-    payment_date = fields.Datetime(string='Payment Date', readonly=1)
-    created_person = fields.Many2one('res.users', string='Created Person', readonly=1)
-    check_date = fields.Datetime(string='Check Date', readonly=1)
-    checked_person = fields.Many2one('res.users', string='Checked Person', readonly=1)
-    validate_date = fields.Datetime(string='Validate Date', readonly=1)
-    validated_person = fields.Many2one('res.users', string='Validated Person', readonly=1)
-
-    voucher_sale_line_ids = fields.One2many('voucher.sale.line', 'account_voucher_id', 'Sale order', readonly=True)
-
-    sale_id = fields.Char(string='Sale Order', compute='_compute_total_sale')
-    amount_sale = fields.Monetary(string='Total', store=False, readonly=False, compute='_compute_total_sale')
-    amount_dathu = fields.Monetary(string='Số tiền đã thu', store=False, readonly=True, compute='_compute_total_sale')
-    amount_con_phaithu = fields.Monetary(string='Số tiền còn phải thu', store=False, readonly=True,
-                                         compute='_compute_total_sale')
-
-
-class voucher_sale_line(models.Model):
-    _name = 'voucher.sale.line'
-
-    order_id = fields.Many2one('sale.order', string='Đơn hàng')
-    order_name = fields.Char(string='Mã SO', related='order_id.name')
-    date_order = fields.Datetime(string='Ngày SO', related='order_id.date_order')
-    amount_total = fields.Float(string='Tổng tiền')
-    so_tien_da_thu = fields.Float(string='Số tiền đã thu', compute=False, store=True)
-    con_phai_thu = fields.Float(string='Số tiền còn phải thu', compute=False)
-    trang_thai_tt = fields.Selection(
-        [('chua_tt', 'Chưa thanh toán'), ('tt_1_phan', 'Thanh toán 1 phần'), ('done_tt', 'Hoàn tất thanh toán')],
-        string='Trạng thái thanh toán', compute=False, store=True)
-    check = fields.Boolean(string='Chọn')
-    account_voucher_id = fields.Many2one('account.voucher')
-
-    @api.depends('order_id.so_tien_da_thu', 'amount_total')
-    def _get_line_data(self):
-        for rec in self:
-            rec.so_tien_da_thu = rec.order_id.so_tien_da_thu
-            rec.con_phai_thu = rec.amount_total - rec.order_id.so_tien_da_thu
-            rec.trang_thai_tt = rec.order_id.trang_thai_tt
+#     amount_input = fields.Float('Số tiền')
+#     note = fields.Char(string='Ghi chú')
+#
+#     journal_id = fcollect_type = fields.Selection([('journal', 'Customer Invoices (VNĐ)')], string='Journal',
+#                                                   default='journal')
+#
+#     payment_journal_id = fields.Many2one('account.journal', string='Payment Method')
+#     account_id = fields.Many2one('account.account', string='Account')
+#
+#     number_voucher = fields.Char(string='Số phiếu thu')
+#     name = fields.Char(string='Payment Reference')
+#     collect_type = fields.Selection([('sale', 'Collect Sale')], string='Collect Type', default='sale')
+#     payment_date = fields.Datetime(string='Payment Date', readonly=1)
+#     created_person = fields.Many2one('res.users', string='Created Person', readonly=1)
+#     check_date = fields.Datetime(string='Check Date', readonly=1)
+#     checked_person = fields.Many2one('res.users', string='Checked Person', readonly=1)
+#     validate_date = fields.Datetime(string='Validate Date', readonly=1)
+#     validated_person = fields.Many2one('res.users', string='Validated Person', readonly=1)
+#
+#     voucher_sale_line_ids = fields.One2many('voucher.sale.line', 'account_voucher_id')
+#
+#
+# class voucher_sale_line(models.Model):
+#     _name = 'voucher.sale.line'
+#
+#     order_name = fields.Char(string='Mã SO')
+#     date_order = fields.Datetime(string='Ngày SO')
+#     amount_total = fields.Float(string='Tổng tiền')
+#     so_tien_da_thu = fields.Float(string='Số tiền đã thu')
+#     con_phai_thu = fields.Float(string='Số tiền còn phải thu')
+#     trang_thai_tt = fields.Selection(
+#         [('chua_tt', 'Chưa thanh toán'), ('tt_1_phan', 'Thanh toán 1 phần'), ('done_tt', 'Hoàn tất thanh toán')],
+#         string='Trạng thái thanh toán')
+#     check = fields.Boolean(string='Chọn')
+#     account_voucher_id = fields.Many2one('account.voucher')
 
 
 class Payments(models.Model):
